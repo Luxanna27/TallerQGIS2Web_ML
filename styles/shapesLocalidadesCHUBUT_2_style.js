aacbdecb6cb3600d2f2e6afa21b93cebf664e8c2
cristianhermo <136131363+cristianhermo@users.noreply.github.com>
2026-06-13 11:13:38 -0300

    Version 1.0 Mapa Web

---
+++ styles/shapesLocalidadesCHUBUT_2_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_shapesLocalidadesCHUBUT_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "16.900000000000002px \'Tahoma\', sans-serif";
+    var labelFill = "#e0dada";
+    var bufferColor = "#ae3b74";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("nombre_geo") !== null) {
+        labelText = String(feature.get("nombre_geo"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(179,92,21,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(34,8,82,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
